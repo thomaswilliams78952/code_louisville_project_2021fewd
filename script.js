7aeaf6e5353657765f1f399a0e7360d64a3a6fcc
Thomas Williams <thomaswilliams@Thomass-MacBook-Air.local>
2021-03-16 22:29:39 -0400

    added javascript to project

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-document.getElementById("email").addEventListener("click", function(){
-    document.querySelector(".email").style.display = "flex";
+document.getElementById("menu").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").style.display = "block";
 })
 
-document.getElementById("join").addEventListener("click", function(){
-    document.querySelector(".email").style.display = "none";
+document.getElementById("a").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").style.display = "none";
 })
 
-document.querySelector(".close").addEventListener("click", function(){
-    document.querySelector(".email").style.display = "none";
+document.getElementById("m").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").style.display = "none";
+})
+
+document.getElementById("l").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").style.display = "none";
+})
+
+document.getElementById("s").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").style.display = "none";
+})
+
+document.querySelector(".close-menu").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").style.display = "none";
 })
